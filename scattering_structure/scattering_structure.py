3aa3848ec8ea262fc7295b1c260d8ca9127dd529
Luis Mickeler <lmickele@uni-muenster.de>
2023-10-04 13:05:44 +0200

    Added poisson disc sampling and empty functions

--- scattering_structure/scattering_structure.py
+++ scattering_structure/scattering_structure.py
@@ -1,18 +1,24 @@
 import numpy as np
+import poisson_disc
+
 
 class ScatteringStructure:
-    def __innit__(self, position, geometry, arrangement, dist, scatterer_size):
+    def __innit__(self, position, geometry: dict, arrangement: str, dist: float, scatterer_radius: float):
         self.center_pos = position
         self.geometry = geometry
         self.arrangement = arrangement
         self.dist = dist
-        self.scatterer_size = scatterer_size
+        self.scatterer_radius = scatterer_radius
 
         # supported arrangement types
         def arr_types(type):
             if type not in ['rectangular', 'tetrahedral', 'random', 'poisson_disc']:
                 raise TypeError(f'The given geometry type {type} is not supported')
         arr_types(geometry['type'])
+
+    # ---------------------------------------
+    # DIFFERENT SCATTERER DISTRIBUTIONS
+    # ---------------------------------------
 
     def create_rectangular_pattern(self):
         max_x = self.geometry['lx']
@@ -41,15 +47,20 @@ class ScatteringStructure:
         positions = np.array(positions)
         return positions
 
-    def create_random_pattern(self, num_points, x_range, y_range):
+    def create_random_pattern(self, num_points):
+        max_x = self.geometry['lx']
+        max_y = self.geometry['ly']
+
         points = []
         while len(points) < num_points:
-            x = np.random.uniform(x_range[0] + self.scatterer_size, x_range[1] - self.scatterer_size)
-            y = np.random.uniform(y_range[0] + self.scatterer_size, y_range[1] - self.scatterer_size)
+            # create random point in given range
+            x = np.random.uniform(0 + self.scatterer_radius, max_x - self.scatterer_radius)
+            y = np.random.uniform(0 + self.scatterer_radius, max_y - self.scatterer_radius)
+            # only as point to list if it doesn't hit rest of the
             valid = True
             for px, py in points:
                 distance = np.sqrt((x - px) ** 2 + (y - py) ** 2)
-                if distance < 2 * self.scatterer_size:
+                if distance < 2 * self.scatterer_radius:
                     valid = False
                     break
             if valid:
@@ -57,5 +68,34 @@ class ScatteringStructure:
         return np.array(points)
 
     def poisson_disc_sampling_pattern(self):
+        max_x = self.geometry['lx']
+        max_y = self.geometry['ly']
+
+        # generate point pattern via Bridson's poisson sampling algorithm
+        # https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
+        points = poisson_disc.Bridson_sampling(radius=self.scatterer_radius, dims=np.array([max_x, max_y]))
+
+        return points
+
+    # ---------------------------------------
+    # REDUCE TO SPECIFIC GEOMETRIES
+    # ---------------------------------------
+
+    def reduce_to_circle(self):
         pass
 
+    def reduce_to_pizza_slice(self):
+        pass
+
+    # ---------------------------------------
+    # VISUALIZATION AND SAVE FUNCTIONS
+    # ---------------------------------------
+
+    def plot_distribution(self):
+        pass
+
+    def plot_scatterer(self):
+        pass
+
+    def save_distribution(self):
+        pass
